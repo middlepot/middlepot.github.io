4c81c8697da1897bf69ebd63238bc3e1190695f0
ഒ <33335240+middlepot@users.noreply.github.com>
2021-07-17 19:05:25 -0300

    +

--- emoji.js
+++ emoji.js
@@ -10,28 +10,21 @@ whiteoverlay.style.top = '0';
 whiteoverlay.style.left = '0';
 whiteoverlay.style.background = '#fff';
 whiteoverlay.style.zIndex = '999999';
-$('body').append(whiteoverlay);
+if(!$('#emojis_whiteoverlay').length)
+	$('body').append(whiteoverlay);
 
 function loadEmojis(){
 	var emojis = ['☂️', '☎️', '✂️', '✉️', '✏️', '❤️', '☕️', '⚰️', '⭐', '🌕', '🌡', '🌧', '🌱', '🌷', '🌹', '🌼', '🍄', '🍓', '🍞', '🍦', '🍩', '🍪', '🍮', '🍯', '🍰', '🍼', '🎀', '🎁', '🎃', '🎄', '🎗', '🎠', '🎻', '🏠', '🐁', '🐇', '🐈', '🐌', '🐑', '🐕', '🐝', '👒', '👗', '👚', '👜', '👶', '💉', '💊', '💌', '💐', '💓', '💕', '💖', '💗', '💘', '💝', '💟', '💻', '💿', '📍', '📚', '📱', '📷', '🔒', '🔪', '🖇', '🖋', '🗝', '🚲', '🛏', '🛼', '🤍', '🥄', '🥛', '🥞', '🥧', '🥿', '🦋', '🦷', '🧁', '🧇', '🧦', '🧴', '🧷', '🧸', '🧺', '🧼', '🩰', '🩳', '🩹', '🪑', '🪦', '🫖'];
-	var vanilla = true;
-	var inner = document.body.innerHTML;
-	if(!inner){
-		inner = $('body').text();
-		vanilla = false;
-	}
-
+	var inner = $('body').html();
 	for(var i = 0; i < emojis.length; ++i){
-		var img = '<img src="https://middlepot.com/emoji/'+emojis[i]+'.gif" style="user-drag: none; -webkit-user-drag: none; user-select: none; -moz-user-select: none; -webkit-user-select: none; -ms-user-select: none;"></img>';
 		var regex = new RegExp(emojis[i], "g");
+		var img = '<img src="https://middlepot.com/emoji/'+emojis[i]+'.gif" style="user-drag: none; -webkit-user-drag: none; user-select: none; -moz-user-select: none; -webkit-user-select: none; -ms-user-select: none;"></img>';
 		inner = inner.replace(regex, img);
 	}
-	if(vanilla)
-		document.body.innerHTML = inner;
-	else
-		$('body').html(inner);
+	inner = inner.replace('loadEmojis();', ''); //remove itself from the code
+	$('body').html(inner);
 }
 
-$(window).load(function() {
-    $('#emojis_whiteoverlay').hide();
+$(document).ready(function() {
+	$('#emojis_whiteoverlay').hide();
 });
